8eb3c9491c995b6908a84f0439dbb06617a13edb
Davit Ghonyan <dghonyan@c2r12s4.42yerevan.am>
2022-07-12 16:56:51 +0400

    complete

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -10,15 +10,20 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
 
-# define GET_NEXT_LINE_H
+# define GET_NEXT_LINE_BONUS_H
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 1
 # endif
 
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdlib.h>
+
 char	*get_next_line(int fd);
-char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strjoin(char *s1, char *s2);
+size_t	ft_strlen(const char *s);
 
 #endif
